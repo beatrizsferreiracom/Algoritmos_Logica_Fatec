c8aa63b9f82fbb201e4646465606ebeb5fbd6b65
Beatriz S. Ferreira <102437407+beatrizsferreiracom@users.noreply.github.com>
2024-02-29 16:25:23 -0300

    Update Aula 02.py

--- Aula 02.py
+++ Aula 02.py
@@ -47,13 +47,6 @@ real = dolar * 4.97
 
 print ('O valor em é real é de: R$' + str(real))
 
-#Extra - Leia do usuário um valor em dólares e converta para real
-
-dolar = float(input('Insira o valor em dólares: US$'))
-real = dolar * 4.97
-
-print ('O valor em é real é de: R$' + str(real))
-
 #7) Leia um número inteiro do usuário na variável N1, Leia outro número do usuário na variável N2, Calcule a soma das 2 variáveis: soma = N1 + N2
 
 N1 = float(input('Insira o valor 1: '))
@@ -66,8 +59,35 @@ print('O total da soma é: ' + str(soma))
 #8) Leia do usuário o tamanho de um lado do quadrado e cácule a área
 
 lado = float(input('Insira o tamanho do lado (em cm): '))
-
 area = lado * 4
 
 print ('A área do quadrado é de ' + str(area) + ' centímetros.')
 
+#9) Leia um número float do usuário na variável Número1, Leia outro número float do usuário variável Número2, Calcule a multiplicação das 2 variáveis: resultado = Número1 * Número2
+
+Número1 = float(input('Insira o valor 1: '))
+Número2 = float(input('Insira o valor 2: '))
+multi = Número1 * Número2
+
+print('O total da multiplicação é: ' + str(multi))
+
+#PB - Leia do usuário um valor em dólares e converta para real
+
+dolar = float(input('Insira o valor em dólares: US$'))
+real = dolar * 4.97
+
+print ('O valor em é real é de: R$' + str(real))
+
+#PC - Leia do usuário o tamanho de um lado do quadrado e cácule a área
+
+lado = float(input('Insira o tamanho do lado (em cm): '))
+area = lado * 4
+print ('A área do quadrado é de ' + str(area) + ' centímetros.')
+
+#PD - Leia do usuário a sua idade e mostre quantas horas já viveu
+
+idade = int(input('Insira a sua idade: '))
+ano = 24 * 365
+horas = idade * ano
+
+print ('Você já viveu aproximadamente ' + str(horas) + ' horas.')
