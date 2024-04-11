6ce65d22431f9349d98b4fe383a07aacefcb53ef
Beatriz S. Ferreira <102437407+beatrizsferreiracom@users.noreply.github.com>
2024-04-11 17:55:53 -0300

    Add files via upload

---
+++ Aula 03/Aula 03 - 29.02.24/Exercício 5.py
@@ -0,0 +1,9 @@
+#5) Perguntar para o usuário o total de reais
+# Calcular quantos centavos são: centavos = reais * 100
+
+reais = int(input('Insira o valor: R$'))
+centavos = reais * 100
+
+print ('O valor em centavos é de: R$' + str(centavos))
+print('=====================================')
+print('                                     ')
---
+++ Aula 03/Aula 03 - 29.02.24/Exercício 6.py
@@ -0,0 +1,8 @@
+#6) Leia do usuário um valor em dólares e converta para real
+
+dolar = float(input('Insira o valor em dólares: US$'))
+real = dolar * 4.97
+
+print ('O valor em é real é de: R$' + str(real))
+print('=====================================')
+print('                                     ')
