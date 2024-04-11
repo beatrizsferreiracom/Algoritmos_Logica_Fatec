882f9ebfcba3ee4e827b0a85f6d7b9069c0dfdc2
Beatriz S. Ferreira <102437407+beatrizsferreiracom@users.noreply.github.com>
2024-04-11 11:53:08 -0300

    Delete Estudo para Prova 1 directory

--- Estudo para Prova 1/Exercício 31.py
+++
@@ -1,3 +0,0 @@
-#31) Escrever um algoritmo que calcule os sucessivos valores de E usando a série abaixo econsiderando primeiro 3 termos, 
-#depois 4 termos e, por fim, 5 termos:
-#E = 1 + 1 / 1! + 1 / 2! + 1 / 3! + 1 / 4!
