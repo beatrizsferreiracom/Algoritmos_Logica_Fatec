7e93fcfca5db595158d35c978112a34edce5caf7
Beatriz S. Ferreira <102437407+beatrizsferreiracom@users.noreply.github.com>
2024-04-11 17:52:54 -0300

    Delete Aula 03 directory

--- Aula 03/Exercício 5.py
+++
@@ -1,9 +0,0 @@
-#5) Perguntar para o usuário o total de reais
-# Calcular quantos centavos são: centavos = reais * 100
-
-reais = int(input('Insira o valor: R$'))
-centavos = reais * 100
-
-print ('O valor em centavos é de: R$' + str(centavos))
-print('=====================================')
-print('                                     ')
