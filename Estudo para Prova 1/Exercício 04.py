3c3144a83d53f73a16c5e69a144a345a9bceb900
Beatriz S. Ferreira <102437407+beatrizsferreiracom@users.noreply.github.com>
2024-04-11 17:32:50 -0300

    Delete Estudo para Prova 1/Exercício 04.py

--- Estudo para Prova 1/Exercício 04.py
+++
@@ -1,7 +0,0 @@
-#4) Faça um algoritmo que leia a idade de uma pessoa expressa em dias e mostre-a expressa em anos, meses e dias.
-
-dias = int(input('Insira a idade em dias: '))
-anos = dias / 365
-meses = dias / 30
-
-print('A idade é de', round(anos), 'anos,', round(meses), 'meses e', dias, 'dias.')
