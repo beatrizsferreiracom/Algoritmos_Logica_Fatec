85ef7e65be029d2462484406176040d76a90290b
Beatriz S. Ferreira <102437407+beatrizsferreiracom@users.noreply.github.com>
2024-04-11 22:35:43 -0300

    Add files via upload

---
+++ Aula 03/Aula 03 - 29.02.24/Exercício 7.py
@@ -0,0 +1,12 @@
+#7) Leia um número inteiro do usuário na variável N1
+# Leia outro número do usuário na variável N2
+# Calcule a soma das 2 variáveis: soma = N1 + N2
+
+N1 = float(input('Insira o valor 1: '))
+N2 = float(input('Insira o valor 2: '))
+
+soma = N1 + N2
+
+print('O total da soma é: ' + str(soma))
+print('=====================================')
+print('                                     ')
---
+++ Aula 03/Aula 03 - 29.02.24/Exercício 9.py
@@ -0,0 +1,11 @@
+#9) Leia um número float do usuário na variável Número1
+# Leia outro número float do usuário variável Número2
+# Calcule a multiplicação das 2 variáveis: resultado = Número1 * Número2
+
+Número1 = float(input('Insira o valor 1: '))
+Número2 = float(input('Insira o valor 2: '))
+multi = Número1 * Número2
+
+print('O total da multiplicação é: ' + str(multi))
+print('=====================================')
+print('                                     ')
