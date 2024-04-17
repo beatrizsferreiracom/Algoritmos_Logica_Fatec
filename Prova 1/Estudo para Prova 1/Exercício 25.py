796a71e86103a6a9ec3180d0548b956411b60830
Beatriz S. Ferreira <102437407+beatrizsferreiracom@users.noreply.github.com>
2024-04-17 20:57:25 -0300

    Update Exercício 25.py

--- Prova 1/Estudo para Prova 1/Exercício 25.py
+++ Prova 1/Estudo para Prova 1/Exercício 25.py
@@ -14,30 +14,32 @@ c = float(input('Insira o valor de c: '))
 if i == 1:
     if a <= b <= c:
         print(a, b, c)
+    elif c <= a <= b:
+        print(c, a, b)
     elif a <= c <= b:
         print(a, c, b)
     elif b <= a <= c:
         print(b, a, c)
     elif b <= c <= a:
         print(b, c, a)
-    elif c <= a <= b:
-        print(c, a, b)
     else:
         print(c, b, a)
+
 
 elif i == 2:
     if a >= b >= c:
         print(a, b, c)
+    elif c >= a >= b:
+       print(c, a, b)
     elif a >= c >= b:
         print(a, c, b)
     elif b >= a >= c:
         print(b, a, c)
     elif b >= c >= a:
         print(b, c, a)
-    elif c >= a >= b:
-        print(c, a, b)
     else:
         print(c, b, a)
+
 
 else:
     if a > b and a > c:
